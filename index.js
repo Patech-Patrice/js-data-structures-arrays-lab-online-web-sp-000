5ce19b02bc01789ad0eb829c9ec7c9d4026cb58e
Patrice Drayton <draytonpatrice@marilynssecret.org>
2018-11-23 20:32:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Write your solution here!
 const drivers = ["Milo", "Otis", "Garfield" ];
 
-function destructivelyAppendDriver.push(name){
-  drivers.push('Ralph');
-  
-}
- 
+function destructivelyAppendDriver(name){
+  drivers.push(name);
+  return drivers;
+  }
+
  
